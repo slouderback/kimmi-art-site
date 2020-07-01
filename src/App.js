ba5650f642567ac54f81ee1216f3154634354747
slouderback <sam@louderback.com>
2020-07-01 00:46:22 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component }  from 'react';
+import './App.css';
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <MainContent />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
